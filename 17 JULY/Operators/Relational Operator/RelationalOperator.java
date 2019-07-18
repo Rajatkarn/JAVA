6f9b32c452ec1479b81d3b925d29d6f2efa5b528
Rajatkarn <51739403+Rajatkarn@users.noreply.github.com>
2019-07-18 18:13:46 +0530

    Create RelationalOperator.java

---
+++ 17 JULY/Operators/Relational Operator/RelationalOperator.java
@@ -0,0 +1,33 @@
+class Demo
+{ 
+    public static void main(String s[])
+	{ 
+	    int a =10 , b= 20 ;
+		if(a<b)
+		{ 
+	      System.out.println("a<b");
+		}
+		if(a<=b)
+		{ 
+	      System.out.println("a<=b");
+		}
+		if(a==b)
+		{ 
+	      System.out.println("a==b");
+		}
+		if(a>b)
+		{
+			System.out.println("a>b");
+		}
+		if(a>=b)
+		{ 
+	      System.out.println();
+		}
+		if(a!=b)
+		{ 
+	      System.out.println("a!=b");
+		}
+	}
+}
+		
+		 
