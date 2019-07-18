b6a88f73d900e8750c4e84465bfa39149f453f95
Rajatkarn <51739403+Rajatkarn@users.noreply.github.com>
2019-07-18 19:20:56 +0530

    Java Core

---
+++ 17 JULY/TypeCasting/TypeCasting.java
@@ -0,0 +1,19 @@
+class Demo 
+{ 
+     public static void main(String ar[])
+	 { 
+	    // This is the Example of Implicit TypeCasting 
+	     int a=10; double b=a ;
+		 System.out.println("This is the Example of Implicit TypeCasting ");
+		 System.out.println("a ="+a);
+		 System.out.println("b ="+b);
+		 
+		 // This is the Example of Explicit TypeCasting 
+	     double c = 12.5 ; int d;
+		 d=(int)c;
+		 System.out.println("This is the Example of Explicit TypeCasting");
+		 System.out.println("c ="+c);
+		 System.out.println("d ="+d);
+	 }
+}
+		 
