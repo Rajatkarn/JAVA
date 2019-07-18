97330c69b88ab59e935d83f09a29d157984bb2ff
Rajatkarn <51739403+Rajatkarn@users.noreply.github.com>
2019-07-18 18:56:24 +0530

    Java Core

---
+++ 17 JULY/Operators/Conditional Operator/ConditionalOperator.java
@@ -0,0 +1,12 @@
+class Demo
+{ 
+  public static void main(String ar[])
+  {
+	 int a = 10 , b = 5;
+	 System.out.println(" ----- TERNARY Operator ----- ");
+	 a=2<3? ++a-b++/a : ++a*--b+a-- ;
+	 System.out.println("a = "+a);
+	 System.out.println("b = "+b);
+  }
+}
+	 
